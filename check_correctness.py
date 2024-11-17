6c9f4804122c66d88a18e0b286a67b40658ed983
megahomyak <g.megahomyak@gmail.com>
2024-11-17 19:31:45 +0500

    Trying to make this

--- check_correctness.py
+++
@@ -1,8 +0,0 @@
-import re
-
-def read(file_name):
-    with open(file_name, encoding="utf-8") as f:
-        return f.read()
-
-all_images_requested = set(re.findall(r'image\("\w+"\)', flags=re.DOTALL))
-read("files/game_resources/script.js")
--- files/game_resources/script.js
+++ files/game_resources/script.js
@@ -1,1 +1,66 @@
-function loadImage() {}
+let imagesToLoad = ["blah.png"];
+let soundsToLoad = [];
+
+function preloadImage(filePath) {
+    let image = new Image();
+    image.addEventListener("load", assetLoaded);
+    image.addEventListener("error", assetFailedToLoad);
+    image.src = filePath;
+}
+function preloadSound(filePath) {
+    let sound = new Audio();
+    sound.autoplay = false;
+    sound.addEventListener("canplaythrough", assetLoaded);
+    sound.addEventListener("error", assetFailedToLoad);
+    sound.src = filePath;
+}
+
+let canvas = document.getElementById("canvas");
+
+let ctx = canvas.getContext("2d");
+function resetCanvas() {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+resetCanvas();
+function writeText(fontSize, text, x, y) {
+    ctx.fillStyle = "white";
+    ctx.font = fontSize + "px sans-serif";
+    ctx.fillText(text, x, y);
+}
+writeText(48, "Loading...", 20, 110);
+
+function drawImage(path, x, y) {
+    return Promise((resolve, reject) => {
+        let image = new Image();
+        image.addEventListener("load", () => {
+            ctx.drawImage(image, x, y);
+            resolve();
+        });
+        image.addEventListener("error", () => reject());
+        image.src = path;
+    });
+}
+
+let assetsLoaded = 0;
+function assetLoaded() {
+    if (imagesToLoad.length + soundsToLoad.length == assetsLoaded) {
+        runGame();
+    }
+    ++assetsLoaded;
+}
+function assetFailedToLoad() {
+    resetCanvas();
+    writeText(20, "Assets failed to load.", 10, 50);
+    writeText(20, "This shouldn't happen.", 10, 80);
+    writeText(20, "Please, contact the dev", 10, 110);
+    writeText(20, "about it.", 150, 140);
+}
+
+for (image of imagesToLoad) {
+    preloadImage(image);
+}
+for (sound of soundsToLoad) {
+    preloadSound(sound);
+}
