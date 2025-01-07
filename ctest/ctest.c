06fc2172413c843a58a349fc817675ecce33b557
megahomyak <g.megahomyak@gmail.com>
2025-01-07 16:09:25 +0500

    Checkpoint before moving to a <table> for buttons

--- ctest/ctest.c
+++ ctest/ctest.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/* Reading the following code goes well with
+https://soundcloud.com/longlivetheanimalscrew/alk-m-e-llta025-alk-m-e-rave-ep-03-alk-m-e-the-raver
+*/
+
 /* Disclaimer: I'm not using NULL here because I think it sucks */
 /* "Game engine code" a.k.a. "game library code": */
 
---
+++ files/game.js
@@ -0,0 +1,3 @@
+/* Reading the following code goes well with
+https://soundcloud.com/longlivetheanimalscrew/alk-m-e-llta025-alk-m-e-rave-ep-03-alk-m-e-the-raver
+*/
